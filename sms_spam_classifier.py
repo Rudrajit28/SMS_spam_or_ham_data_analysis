e44146f27628dab2b318639209164dddf425d018
Rudrajit28 <153164299+Rudrajit28@users.noreply.github.com>
2026-05-01 23:49:52 +0530

    sms_spam_classifier.py

--- sms_spam_classifier.py
+++ sms_spam_classifier.py
@@ -64,3 +64,34 @@ sns.histplot(df[df['target']==1]['num_characters'],color='red')
 sns.pairplot(df,hue='target')
 #plt.show()
 sns.heatmap(df.corr(),annot=True)
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stopwords.words('english')
+from nltk.stem.porter import PorterStemmer
+nltk.data.path.append('C:\\Users\\Rudra\\AppData\\Roaming\\nltk_data')
+import string
+string.punctuation
+
+def transform_text(text):
+    text=text.lower()
+    text=nltk.word_tokenize(text)
+    ps=PorterStemmer()
+    y=[]
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text=y[:]
+    y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    text=y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+    return " ".join(y)
+
+
+df['transformed_text']=df['text'].apply(transform_text)
+print(df.head(5))
